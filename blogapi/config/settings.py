80f841a6029e0e10d18a58dee26133a8bab6db4b
Aldo Matus <aldo.matus@outlook.com>
2021-06-29 09:02:14 -0500

    Token Authentication implemented

--- blogapi/config/settings.py
+++ blogapi/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     #   3rd party apps
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 # REST_FRAMEWORK = {
@@ -51,10 +52,20 @@ INSTALLED_APPS = [
 # ]
 # }
 
+# REST_FRAMEWORK = {
+# 'DEFAULT_PERMISSION_CLASSES': [
+# 'rest_framework.permissions.IsAuthenticated',
+# ]
+# }
+
 REST_FRAMEWORK = {
-'DEFAULT_PERMISSION_CLASSES': [
-'rest_framework.permissions.IsAuthenticated',
-]
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
 
 
